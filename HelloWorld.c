ed8c5a5e24651d5b864c19a00bec747dcb889ce4
pchp250g8-home <pchp250g8@tutamail.com>
2025-12-01 14:06:42 +0300

    Created Conio Application

---
+++ HelloWorld.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <tchar.h>
+#include <conio.h>
+
+int _tmain(int argc, _TCHAR* argv[]) 
+{
+   _setcursortype(_NOCURSOR);
+   clrscr();
+   gotoxy(10,10);
+   cprintf("%s","Hello,World!!!");
+   getch();
+}
